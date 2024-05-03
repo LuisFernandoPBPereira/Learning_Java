6d93f46b39121877d2069ccf08509be374a2b35e
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-05-03 09:41:03 -0300

    Recebendo dados via console

--- POO/Pessoa/src/App.java
+++ POO/Pessoa/src/App.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Aluno pessoa = new Aluno(1, "CC");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nome = scanner.next();
+        int idade = scanner.nextInt();
+        double altura = scanner.nextDouble();
+        int matricula = scanner.nextInt();
+        String curso = scanner.next();
+
+        scanner.close();
+
+        Aluno pessoa = new Aluno(nome, idade, altura, matricula, curso);
 
         pessoa.exibirInformacoes();
 
